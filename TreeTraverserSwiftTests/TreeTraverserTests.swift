f1bfe97125c54fc37f82d182e96fe405292c04a6
beepscore <support@beepscore.com>
2017-05-20 12:41:16 -0700

    Add testNodesByTraversingZigZagFourLevelsSomeNodesNil

--- TreeTraverserSwiftTests/TreeTraverserTests.swift
+++ TreeTraverserSwiftTests/TreeTraverserTests.swift
@@ -50,4 +50,43 @@ class TreeTraverserTests: XCTestCase {
         XCTAssertEqual(actual.description, "[0, 8, 2, 7, -3, 12, 20]")
     }
 
+    func testNodesByTraversingZigZagFourLevelsSomeNodesNil() {
+        //              5
+        //            /  \
+        //           /    \
+        //          /      \
+        //         4        7
+        //        / \      /
+        //       2   9    6
+        //          / \
+        //         8   11
+        //
+        let node12 = nil as Node?
+        let node11 = nil as Node?
+        let node10 = Node(value: 11, left: nil, right: nil)
+        let node9 = Node(value: 8, left: nil, right: nil)
+        let node8 = nil as Node?
+        let node7 = nil as Node?
+        let node6 = nil as Node?
+        let node5 = Node(value: 6, left: node11, right: node12)
+        let node4 = Node(value: 9, left: node9, right: node10)
+        let node3 = Node(value: 2, left: node7, right: node8)
+        let node2 = Node(value: 7, left: node5, right: node6)
+        let node1 = Node(value: 4, left: node3, right: node4)
+        let node0 = Node(value: 5, left: node1, right: node2)
+
+        let actual = TreeTraverser.nodesByTraversingZigZag(root: node0)
+        XCTAssertEqual(actual.count, 8)
+
+        XCTAssertEqual(nodeValues(actual),
+                       [node0.value,
+                        node2.value, node1.value,
+                        node3.value, node4.value, node5.value,
+                        node10.value, node9.value])
+
+        XCTAssertEqual(nodeValues(actual), [5, 7, 4, 2, 9, 6, 11, 8])
+        XCTAssertEqual(actual.description, "[5, 7, 4, 2, 9, 6, 11, 8]")
+    }
+    
+
 }
