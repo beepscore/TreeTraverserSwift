1f2462b34431a960fa17261019961b63f0b8dd04
beepscore <support@beepscore.com>
2017-05-22 08:56:33 -0700

    Convert project from single view app to framework for simplicity.
    Procedure-
    In Xcode delete targets for TreeTraverserSwift app and tests and all related files except xcodeproj. Add targets for framework and tests, changing name to TreeTraverser.
    Copy source code files like Node.swift and TreeTraverser.swift from app into framework.

---
+++ TreeTraverser/TreeTraverser.h
@@ -0,0 +1,19 @@
+//
+//  TreeTraverser.h
+//  TreeTraverser
+//
+//  Created by Steve Baker on 5/22/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TreeTraverser.
+FOUNDATION_EXPORT double TreeTraverserVersionNumber;
+
+//! Project version string for TreeTraverser.
+FOUNDATION_EXPORT const unsigned char TreeTraverserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreeTraverser/PublicHeader.h>
+
+
--- TreeTraverserTests/NodeTests.swift
+++ TreeTraverserTests/NodeTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import TreeTraverserSwift
+@testable import TreeTraverser
 
 class NodeTests: XCTestCase {
 
